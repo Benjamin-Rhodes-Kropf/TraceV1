78afbf0e805c0186186ad954ab999bf383568631
Ben Rhodes-Kropf <85074410+Benjamin-Rhodes-Kropf@users.noreply.github.com>
2022-10-23 20:12:03 -0400

    0.2.6
    -stripped down fb auth

--- Trace/Assets/Main/Scripts/ScreenScripts/AddFriendsUIManager.cs
+++ Trace/Assets/Main/Scripts/ScreenScripts/AddFriendsUIManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Mapbox.Json.Bson;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,32 +10,49 @@ using UnityEngine.UI;
 public class AddFriendsUIManager : MonoBehaviour
 {
     [SerializeField] private TMP_InputField friendSearchBar;
+    [SerializeField] private string searchedUserID;
+    [SerializeField] private Texture searchedUserProfilePhoto;
+    [SerializeField] private RawImage displayUserPhoto;
     void OnDisable()
     {
+        searchedUserID = "";
         Debug.Log("AddFriendsUIManager: script was disabled");
     }
 
     void OnEnable()
     {
+        searchedUserID = "";
         Debug.Log("AddFriendsUIManager: script was enabled");
         friendSearchBar.text = "";
     }
     
     public void SearchForUser()
     {
-        StartCoroutine(FirebaseManager.instance.SearchForUserIDByUsername(friendSearchBar.text, (myReturnValue) =>
+        StartCoroutine(FirebaseManager.instance.SearchForUserByUsername(friendSearchBar.text, (myReturnValue) =>
         {
             if (myReturnValue.IsSuccessful)
             {
+                Debug.Log("search returned user");
                 Debug.Log(myReturnValue.ReturnValue);
+                searchedUserID = myReturnValue.message.ToString();
+                searchedUserProfilePhoto = (Texture2D)myReturnValue.ReturnValue;
+                displayUserPhoto.texture = searchedUserProfilePhoto;
             }
             else
             {
                 Debug.LogWarning("failed to find user");
             }
         }));
-
     }
+
+    public void RequestFriendWithSearchedUser()
+    {
+        //if there is no user
+        //return with somthin along the lines of "you have to search a valid user first"
+        //else
+        //call firebase manager request friendship
+    }
+    
 
 
     // Update is called once per frame
